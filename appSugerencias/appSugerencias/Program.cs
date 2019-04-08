e5c8986ad55d8c71982932dd20f698a3fd6695d2
DaNxD0003 <45467426+DaNxD0003@users.noreply.github.com>
2019-04-08 13:20:07 -0500

    VALIDA VENTANAS

--- appSugerencias/appSugerencias/Program.cs
+++ appSugerencias/appSugerencias/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Reflection;
 
 using System.Windows.Forms;
 
@@ -8,15 +10,42 @@ namespace appSugerencias
 {
     static class Program
     {
+        private static bool FirstInstance
+        {
+            get
+            {
+                bool created;
+                string name = Assembly.GetEntryAssembly().FullName;
+                // created will be True if the current thread creates and owns the mutex.
+                // Otherwise created will be False if a previous instance already exists.
+
+                Mutex mutex = new Mutex(true, name, out created);
+                return created;
+            }
+        }
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
+
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Login());
+            if (FirstInstance)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frm_Login());
+            }
+            else
+            {
+
+                MessageBox.Show("La apliacion ya esta Ejecutandose");
+                Application.Exit();
+            }
+
         }
     }
+
+
 }
